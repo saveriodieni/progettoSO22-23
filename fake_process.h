0884dfe7c63b41a89e351fcf3663862773cd11f6
saverio dieni <saveriodieni@gmail.com>
2023-06-15 21:17:31 +0200

    modifica fake_process

---
+++ fake_process.h
@@ -0,0 +1,29 @@
+#pragma once
+#include "linked_list.h"
+
+typedef enum {CPU=0, IO=1} ResourceType;
+
+// event of a process, is in a list
+typedef struct {
+  ListItem list;
+  ResourceType type;
+  int duration;
+} ProcessEvent;
+
+typedef struct {
+  ProcessEvent proc_ev;
+  int paused; //added by me
+} CPUProcessEvent;
+
+// fake process
+typedef struct {
+  ListItem list;
+  int pid; // assigned by us
+  int arrival_time;
+  int prev_q; //added by me
+  ListHead events;
+} FakeProcess;
+
+int FakeProcess_load(FakeProcess* p, const char* filename);
+
+int FakeProcess_save(const FakeProcess* p, const char* filename);
